f329f2c5ee890b5b37a4110a80abc4898e0f6376
Fahrstuhl <fahrstuhl@users.noreply.github.com>
2020-09-01 17:42:30 +0200

    adds example code

---
+++ image_file_loader.gd
@@ -0,0 +1,30 @@
+tool
+extends ResourceFormatLoader
+class_name ImageFileLoader
+
+var official_image_file_endings = ["bmp", "dds", "exr", "hdr", "jpg", "jpeg", "png", "tga", "svg", "svgz", "webp"]
+
+func handles_type(typename: String):
+	return typename == "ImageTexture"
+
+func load(path, original_path):
+	print("Custom loader called.")
+	var image = Image.new()
+	var texture = ImageTexture.new()
+	var err = image.load(path)
+	if err != OK:
+		printerr("Can't load image.")
+		return err
+	else:
+		texture.create_from_image(image)
+		texture.take_over_path(path) # this makes sure the RichTextLabel code finds the image resource it needs already loaded
+		return texture
+
+func get_resource_type(path):
+	if path.get_extension() in official_image_file_endings:
+		return "ImageTexture"
+	else:
+		return ""
+
+func get_recognized_extensions():
+	return PoolStringArray(official_image_file_endings)
---
+++ main.gd
@@ -0,0 +1,26 @@
+tool
+extends RichTextLabel
+
+# needs a new variable so the setter is called (you can't override parent setters in Godot)
+export(String, MULTILINE) var bbcode_with_images = "" setget set_bbcode_with_images
+var textures = []
+
+# the setter loads the images and sets bbcode_text afterwards
+func set_bbcode_with_images(value):
+	bbcode_with_images = value
+	# loading all necessary images needs to be done before setting the bbcode_text
+	load_images_in_text()
+	# setting the bbcode_text makes the RichTextLabel try and load necessary images
+	bbcode_text = value
+
+func load_images_in_text():
+	var re = RegEx.new()
+	# this finds all img tags in the bbcode and makes the image path easily accessible in code
+	re.compile("\\[img.*\\](?P<path>.*)\\[\\/img\\]")
+	var imgs = re.search_all(bbcode_with_images)
+	for img in imgs:
+		var path = img.get_string("path")
+		# this calls our custom loader to load the image resource into the resource cache
+		var texture = ResourceLoader.load(path, "ImageTexture")
+		# the resource seems to need to be persistent, so we store it in the textures attribute
+		textures.append(texture)
